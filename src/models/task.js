04baece1224787ab1f62bba7441018f337b54a39
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-31 14:32:06 -0600

    Add 'owner' property to Task model.

--- src/models/task.js
+++ src/models/task.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const taskSchema = new mongoose.Schema({
   description: String,
   completed: Boolean,
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 module.exports = mongoose.model('Task', taskSchema);
