ab2cc052931863dd71fa62bda58ab4763eea3e98
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-31 14:33:16 -0600

    Add the _id of the user to the 'owner' property of a task at POST /tasks.

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,7 +5,9 @@ const auth = require('../src/middleware/auth');
 const router = express.Router();
 
 router.post('/', auth, (req, res, next) => {
-  res.status(201).json(new Task(req.body));
+  const task = new Task(req.body);
+  task.owner = res.locals.user._id;
+  res.status(201).json(task);
 });
 
 module.exports = router;
