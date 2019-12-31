acd33a470012bd04e8ae5c28b2498253287012e0
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-31 11:56:56 -0600

    Add error-handling logic to auth middleware when an invalid JWT is passed.
    I want to always throw a ResponseError when something goes wrong,
    and sometimes I can do that in a guard condition with an if() statement
    but sometimes a third-party library just throws it's own error
    and I have to intercept it in the catch() handler and change the
    error object to my ResponseError object, as I did here.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,18 +1,21 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const loadConfig = require('../utils/loadConfig');
-const ResponeError = require('../utils/responseError');
+const ResponseError = require('../utils/responseError');
 
 module.exports = async (req, res, next) => {
   try {
     const config = await loadConfig();
-    if (!req.get('Authorization')) throw new ResponeError(401, 'Must include a JWT in an Authorization header');
+    if (!req.get('Authorization')) throw new ResponseError(401, 'Must include a JWT in an Authorization header');
     const token = req.get('Authorization').replace('Bearer ', '');
     const _id = jwt.verify(token, config.secret)._id;
     const user = await User.findById(_id);
     res.locals.user = user;
     next();
   } catch (e) {
+    if (e.message.includes('invalid signature')) {
+      e = new ResponseError(401, 'Must include a valid JWT');
+    }
     next(e);
   }
 };
