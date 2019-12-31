36a06acdca44c66f9188d781a565935dd5a6fb98
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-30 23:07:30 -0600

    Throw an error if the wrong content-type is sent at POST /user.

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/', async (req, res, next) => {
   try {
+    if (req.get('Content-Type') !== 'application/json') throw new Error('To create a user, the request body must have the `Content-Type: application/json` header');
     const user = await User.create(req.body);
     res.status(201).json(user.profile);
   } catch (e) {
