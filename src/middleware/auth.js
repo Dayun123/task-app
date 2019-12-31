db4ac84fe5c98890404ed9e75dc335d6856c4833
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-31 11:54:05 -0600

    Change wording of error message when no JWT is passed in.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,7 +6,7 @@ const ResponeError = require('../utils/responseError');
 module.exports = async (req, res, next) => {
   try {
     const config = await loadConfig();
-    if (!req.get('Authorization')) throw new ResponeError(401, 'Must include a valid JWT');
+    if (!req.get('Authorization')) throw new ResponeError(401, 'Must include a JWT in an Authorization header');
     const token = req.get('Authorization').replace('Bearer ', '');
     const _id = jwt.verify(token, config.secret)._id;
     const user = await User.findById(_id);
