456879bef40d074fb779887225d473b2539591ed
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-30 21:55:30 -0600

    Create basic User model and return a User in the response to POST /user.

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const User = require('../src/models/user');
+
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
-  res.status(201).json({ user: req.body });
+  res.status(201).json({ user: new User(req.body) });
 });
 
 module.exports = router;
