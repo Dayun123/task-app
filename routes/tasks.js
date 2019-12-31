0bb267881357599ec8d887a373939b0f19609c43
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-31 14:08:32 -0600

    Create a basic Task model and return a task at POST /tasks.

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const Task = require('../src/models/task');
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
-  res.status(201).json(req.body);
+  res.status(201).json(new Task(req.body));
 });
 
 module.exports = router;
