701cf38cedbe4ab8d62a1f4c70d16e1ef3c955cc
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-31 11:47:34 -0600

    Refactor validateContentType to use try..catch.
    Even though this doesn't change the behavior at this point, it
    makes sense to use try..catch then pass the error to next(e) in
    this middleware as I'm doing in auth.

--- src/middleware/validateContentType.js
+++ src/middleware/validateContentType.js
@@ -1,8 +1,10 @@
 const ResponseError = require('../utils/responseError');
 
 module.exports = (req, res, next) => {
-  if (req.get('Content-Type') !== 'application/json') {
-    throw new ResponseError(415, 'To create a user, the request body must have the `Content-Type: application/json` header');
+  try {
+    if (req.get('Content-Type') !== 'application/json') throw new ResponseError(415, 'To create a user, the request body must have the `Content-Type: application/json` header');
+    next();
+  } catch (e) {
+    next(e);
   }
-  next();
 };
