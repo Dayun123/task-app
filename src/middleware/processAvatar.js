e650a405649530c2f219fa0fe9a3813c77fb90f7
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-01 10:55:48 -0600

    Resize avatar and convert to png before storing at user.avatar.

--- src/middleware/processAvatar.js
+++ src/middleware/processAvatar.js
@@ -1,10 +1,12 @@
+const sharp = require('sharp');
 const ResponseError = require('../utils/responseError');
 
 module.exports = async (req, res, next) => {
   console.log(req.file);
   try {
     if (!req.file.mimetype.startsWith('image')) throw new ResponseError(415, 'Avatars must be images');
-    res.locals.user.avatar = req.file.buffer;
+
+    res.locals.user.avatar = await sharp(req.file.buffer).resize(100, 100).png().toBuffer();
     await res.locals.user.save();
     next();
   } catch (e) {
