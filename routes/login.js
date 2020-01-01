bf6e15e8d94b2a43aef0b0a143e7ef5b99b3362d
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-31 20:56:28 -0600

    Pass a ResponseError to the app-level error-handling middleware via next() at POST /login.

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../src/models/user');
+const ResponseError = require('../src/utils/responseError');
 
 const router = express.Router();
 
@@ -8,7 +9,7 @@ router.post('/', async (req, res, next) => {
     const user = await User.auth(req.body);
     res.status(200).json(user.profile);
   } catch (e) {
-    next(e);
+    next(new ResponseError(400, e.message));
   }
 });
 
