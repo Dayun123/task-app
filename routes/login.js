3376e823c5b160f8544da1300ffa98a49c4a4cb2
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-31 20:50:28 -0600

    Add try..catch to POST /login route-handler.

--- routes/login.js
+++ routes/login.js
@@ -4,8 +4,12 @@ const User = require('../src/models/user');
 const router = express.Router();
 
 router.post('/', async (req, res, next) => {
-  const user = await User.auth(req.body);
-  res.status(200).json(user.profile);
+  try {
+    const user = await User.auth(req.body);
+    res.status(200).json(user.profile);
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
